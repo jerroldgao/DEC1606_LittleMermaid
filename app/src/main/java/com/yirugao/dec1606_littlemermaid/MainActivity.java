0d6f86165ee41fd92a36574c1f260eb247444ddd
gaoyr <gaoyr@iastate.edu>
2016-04-10 14:48:42 -0500

    Initial menu screen

--- app/src/main/java/com/yirugao/dec1606_littlemermaid/MainActivity.java
+++ app/src/main/java/com/yirugao/dec1606_littlemermaid/MainActivity.java
@@ -2,12 +2,26 @@ package com.yirugao.dec1606_littlemermaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button launchButton = (Button) findViewById(R.id.launchButton);
+    Button receiveButton = (Button) findViewById(R.id.receiveButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.main_activity_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
