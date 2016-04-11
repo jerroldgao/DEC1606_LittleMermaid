f2edae72e0677a55c1545017a966e7c0094b7cdd
gaoyr <gaoyr@iastate.edu>
2016-04-10 19:04:22 -0500

    initial Setting, recording, and profile page. Make navigation ready.

--- app/src/main/java/com/yirugao/dec1606_littlemermaid/MainActivity.java
+++ app/src/main/java/com/yirugao/dec1606_littlemermaid/MainActivity.java
@@ -1,20 +1,27 @@
 package com.yirugao.dec1606_littlemermaid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button launchButton = (Button) findViewById(R.id.launchButton);
-    Button receiveButton = (Button) findViewById(R.id.receiveButton);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button launchButton = (Button) findViewById(R.id.launchButton);
+        Button receiveButton = (Button) findViewById(R.id.receiveButton);
+        MenuItem profileButton = (MenuItem) findViewById(R.id.action_profile);
+        MenuItem settingButton = (MenuItem) findViewById(R.id.action_settings);
+        MenuItem recordingButton = (MenuItem) findViewById(R.id.action_record);
 
 
     }
@@ -24,4 +31,32 @@ public class MainActivity extends AppCompatActivity {
         inflater.inflate(R.menu.main_activity_bar, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                startActivity(new Intent(MainActivity.this,SettingActivity.class));
+                return true;
+
+            case R.id.action_record:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite..
+                startActivity(new Intent(MainActivity.this,RecordingActivity.class));
+                return true;
+
+            case R.id.action_profile:
+                startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/yirugao/dec1606_littlemermaid/SettingActivity.java
@@ -0,0 +1,30 @@
+package com.yirugao.dec1606_littlemermaid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; goto parent activity.
+                this.finish();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
