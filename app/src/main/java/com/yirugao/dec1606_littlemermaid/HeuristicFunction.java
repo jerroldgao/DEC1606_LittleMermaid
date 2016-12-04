6f5318252f836eb2b0d37ea1caa10a85de2cca34
jerroldgao <jerrold.gao@gmail.com>
2016-12-04 12:22:24 -0600

    function change

--- app/src/main/java/com/yirugao/dec1606_littlemermaid/HeuristicFunction.java
+++ app/src/main/java/com/yirugao/dec1606_littlemermaid/HeuristicFunction.java
@@ -10,10 +10,17 @@ public class HeuristicFunction implements Comparator<Word>{
 
     @Override
     public int compare(Word lhs, Word rhs) {
-        double leftCate = 1/((double)lhs.getIndex()+1)*lhs.getCategoryValue();
-        double rightCate = 1/((double)rhs.getIndex()+1)*rhs.getCategoryValue();
-        double leftNgram = ((double)lhs.getIndex())/((double)lhs.getIndex()+1)*lhs.getNgramValue();
-        double rightNgram = ((double)rhs.getIndex())/((double)rhs.getIndex()+1)*rhs.getNgramValue();
-        return (int)(leftCate+leftNgram-rightCate-rightNgram);
+        double leftFunc = Math.exp((-0.25)*((double)(lhs.getIndex()+1)));
+        double rightFunc = Math.exp((-0.25)*((double)(lhs.getIndex()+1)));
+        double leftCate = leftFunc*lhs.getCategoryValue();
+        double rightCate = rightFunc*rhs.getCategoryValue();
+        double leftNgram = (1-leftFunc)*lhs.getNgramValue();
+        double rightNgram = (1-rightFunc)*rhs.getNgramValue();
+        double result = leftCate+leftNgram-rightCate-rightNgram;
+        if (result >= 0){
+            return 1;
+        }else {
+            return -1;
+        }
     }
 }
